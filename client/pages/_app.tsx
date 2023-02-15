22f8eaab0f8531e8491a84707585e1c4e404b33d
prajnastra <coder.abhijit90@gmail.com>
2023-02-15 12:43:22 +0530

    add: react-query

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,23 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { theme } from '../theme'
+
+const queryClient = new QueryClient()
+const NODE_ENV = process.env.NODE_ENV || 'development'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+
+        {NODE_ENV !== 'production' && (
+          <ReactQueryDevtools initialIsOpen={false} />
+        )}
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
