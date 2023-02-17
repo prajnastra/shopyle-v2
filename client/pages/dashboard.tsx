4148307c309702522820b2dfde590ff23f1f4d2c
prajnastra <coder.abhijit90@gmail.com>
2023-02-17 12:41:35 +0530

    update: dashboard

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -3,7 +3,25 @@ import type { NextPage, GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { signOut, getSession } from 'next-auth/react'
 
+import {
+  Container,
+  SimpleGrid,
+  Alert,
+  AlertIcon,
+  Text,
+  IconButton,
+} from '@chakra-ui/react'
+import {
+  SettingsIcon,
+  InfoIcon,
+  CalendarIcon,
+  CopyIcon,
+} from '@chakra-ui/icons'
+
+import Card from '../components/Card'
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import ProfileCard from '../components/ProfileCard'
 
 import { SessionExtended } from '../types'
 
@@ -12,6 +30,7 @@ interface DashboardProps {
 }
 
 const Dashboard: NextPage<DashboardProps> = ({ session }) => {
+  console.log(session)
   return (
     <>
       <Head>
@@ -21,8 +40,59 @@ const Dashboard: NextPage<DashboardProps> = ({ session }) => {
       </Head>
       <main>
         <Navbar session={session} signOut={signOut} />
-        <h1>Dashboard</h1>
+        <Container maxW={'7xl'} py={10} px={8}>
+          <ProfileCard
+            name={session.user.name || ''}
+            user_type={session.user.user_type}
+          />
+
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10}>
+            <Card title="Settings" Icon={SettingsIcon}>
+              <Alert status="info">
+                <AlertIcon />
+                This feature will be added soon...
+              </Alert>
+            </Card>
+
+            <Card title="Profile details" Icon={InfoIcon}>
+              <Text fontWeight={600} color={'gray.500'}>
+                Id: {session.user.id}
+                <IconButton
+                  aria-label="Copy id"
+                  icon={<CopyIcon />}
+                  size="xs"
+                  ml={2}
+                  onClick={() => {
+                    navigator.clipboard.writeText(session.user.email)
+                  }}
+                />
+              </Text>
+
+              <Text fontWeight={600} color={'gray.500'}>
+                Email: {session.user.email}
+                <IconButton
+                  aria-label="Copy email"
+                  icon={<CopyIcon />}
+                  size="xs"
+                  ml={2}
+                  onClick={() => {
+                    navigator.clipboard.writeText(session.user.email)
+                  }}
+                />
+              </Text>
+            </Card>
+
+            <Card title="Activities" Icon={CalendarIcon}>
+              <Alert status="info">
+                <AlertIcon />
+                This feature will be added soon...
+              </Alert>
+            </Card>
+          </SimpleGrid>
+        </Container>
       </main>
+
+      <Footer />
     </>
   )
 }
